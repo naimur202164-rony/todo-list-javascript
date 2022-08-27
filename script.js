ba30c7093214239ff45556f044c2f8036c122158
Your Name <naimur202163@gmail.com>
2022-08-27 15:08:22 +0600

    working on todo

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 console.log("Working on todo list");
 
-const addItems=document.querySelector('.btn');
-console.log(addItems)
+const addItems = document.querySelector(".btn");
+let p = document.querySelector(".two p");
+
+const getValue=()=>{
+    let val = document.querySelector(".inputs").value;
+}
+
+console.log(input);
+
+const hello=()=>{
+    addItems.addEventListener('click',(e)=>{
+ })
+}
+
